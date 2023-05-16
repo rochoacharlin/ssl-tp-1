2ab41f33a0b05be9ca9ca3e9f0e4b989e54b850e
nehuen-b-a <nehuen-b-a>
2023-05-16 01:19:19 -0300

    PuntoF

---
+++ TP1.md
@@ -0,0 +1,23 @@
+
+# SINTAXIS Y SEMÁNTICA DE LOS LENGUAJES
+## K2032 - 2023
+### TRABAJO PRÁCTICO N° 1
+#### Lenguaje C
+
+ *1 Realizar en Lenguaje C una función para realizar cada una de las operaciones solicitadas, sin la utilización de funciones de biblioteca:*
+
+- a Calcular y retornar la longitud de una cadena dada.
+
+- b Convertir una cadena de dígitos en su equivalente numérico.
+
+- c Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula.
+
+- d Eliminar de una cadena dada todas las ocurrencias de un carácter dado.
+
+- e Concatenar al final de la primera cadena dada una segunda cadena también dada.
+
+- f Modificar la cadena dada con la inserción de un carácter dado en una posición determinada.
+
+*-2 Construir un programa para el testeo de las funciones del punto 1.*
+
+Fecha límite de entrega: 27 de mayo
---
+++ puntoF.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+//f) Modificar la cadena dada con la inserción de un carácter dado en una posición determinada.
+
+int longitudDeCadena (char* cadena);
+void insertarCharEnPos(char *str, char caracter, int pos);
+
+int main()
+{
+    char cadena[] = "HolaMundo!";
+    char caracter =' ';
+    int pos = 4;
+    
+    printf("Cadena original: %s\n Caracter insertado \" \" , en la poscicion %d \n", cadena, pos);
+    insertarCharEnPos(cadena, caracter, pos);
+
+    return 0;
+}
+
+// Use la logica del ejercicio 1, estaria bueno meter todos los ejercicios en un "biblioteca"
+
+ int longitudDeCadena (char* cadena) 
+ {
+    int contador=0;
+    while(cadena[contador]!='\0'){
+        contador++;
+    }
+    return contador;
+}
+
+// como no tengo acceso a stdlib.h, no puedo reservar memoria para el array con mayor tamaño
+// por lo tanto me quede que sin efecto en esta funcion, por lo mismo imprimo la cadena dentro de la misma
+
+void insertarCharEnPos(char *cadena, char caracter, int pos)
+{
+    int longAux = longitudDeCadena(cadena)+1;
+    printf(" %d \n", longAux);
+    char aux[longAux];
+
+    for (int i = 0; i <= longAux; i++)
+    {
+        *(aux+i) = i < pos? *(cadena+i) : i == pos? caracter : *(cadena+i-1);
+    }
+        
+    printf("Cadena con un espacio insertado en la poscicion 4: %s\n", aux);
+}
