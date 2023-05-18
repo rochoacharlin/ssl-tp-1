cc2564263400aca4f891f543d4416717da36e681
NicolasKalaydjian <nicolasrodrigo1199@gmail.com>
2023-05-18 03:45:45 -0300

    detalles finales

--- puntoF.c
+++ puntoF.c
@@ -1,19 +1,9 @@
 // f. Modificar la cadena dada con la inserción de un carácter dado en una posición determinada.
 #include <stdio.h>
 
-int longitudString(char* cadena)
-{
-    int contador=0;
-    while(cadena[contador]!='\0')
-    {
-        contador++;
-    }
-    return contador;
-}
-
 void insertarCharEnPos(char *cadena, char caracter, int pos)
 {
-    int longAux = longitudString(cadena)+1;
+    int longAux = longitudCadena(cadena)+1;
     char aux[longAux];
 
     for (int i = 0; i <= longAux; i++)
